8eb669b60adb8988daec4a2ad5d17e06ea2d7cd4
Saaoz <bastie_nb1998@hotmail.fr>
2025-01-29 02:31:04 +0100

    test de patch pour les erreurs de texture block qui a peut-être fonctionner mais gros problème d'enregistrement du block va fabric ( pas encore tester la version forge pour savoir si elle bug ou non mais pas d'erreur à l'affichage sachant que pour le moment c'est un simple enregistrement de bloc ni craft ni rien

--- common/src/main/java/org/ac/algocity/block/ModBlocks.java
+++ common/src/main/java/org/ac/algocity/block/ModBlocks.java
@@ -3,7 +3,10 @@ package org.ac.algocity.block;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MapColor;
+
+
+import static net.minecraft.data.models.model.TextureSlot.DIRT;
 
 public class ModBlocks {
     // ID de notre bloc
@@ -12,9 +15,10 @@ public class ModBlocks {
     // Le bloc lui-même
     public static Block STONE_DIRT;
 
-    // Propriétés du bloc
+    // Proprietors du bloc
     public static final BlockBehaviour.Properties STONE_DIRT_PROPERTIES = BlockBehaviour.Properties
-            .of(Material.DIRT)
+            .of()
+            .mapColor(MapColor.DIRT)
             .strength(0.5F)
             .sound(SoundType.STONE);
 }
--- fabric/src/main/java/org/ac/algocity/FabricModBlocks.java
+++ fabric/src/main/java/org/ac/algocity/FabricModBlocks.java
@@ -1,31 +1,30 @@
 package org.ac.algocity;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.Registry;
-import net.minecraft.core.registries.Registries;
-
-import net.minecraft.util.Identifier;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
 import org.ac.algocity.block.ModBlocks;
 
-
 public class FabricModBlocks extends ModBlocks {
     public static void register() {
+        // Création du bloc
+        STONE_DIRT = new Block(STONE_DIRT_PROPERTIES);
 
+        // Enregistrement du bloc
+        Registry.register(
+                BuiltInRegistries.BLOCK,
+                new ResourceLocation("algocity", STONE_DIRT_ID),
+                STONE_DIRT
+        );
+
+        // Enregistrement de l'item du bloc
+        Registry.register(
+                BuiltInRegistries.ITEM,
+                new ResourceLocation("algocity", STONE_DIRT_ID),
+                new BlockItem(STONE_DIRT, new FabricItemSettings())
+        );
     }
 }
-
-
-
-//public class FabricModBlocks extends ModBlocks {
-//    public static void register() {
-//        // Création du bloc
-//        STONE_DIRT = new Block(STONE_DIRT_PROPERTIES);
-//
-//        // Enregistrement du bloc
-//        Registry.register(
-//                Registries.BLOCK,
-//                new Identifier("algocity", STONE_DIRT_ID),
-//                STONE_DIRT
-//        );
-//    }
-//}
